85dc9c6a199aa70344ac3338e8128bf073f1ad0f
Dawid Janik <dawid.janik95@gmail.com>
2020-09-18 17:54:15 +0200

    fix(enum): Change alias for custom enum type - closes #2.

--- src/Console/GenerateFactoryCommand.php
+++ src/Console/GenerateFactoryCommand.php
@@ -76,7 +76,7 @@ class GenerateFactoryCommand extends Command
         $this->files = $files;
         $this->view = $view;
 
-        Type::addType('enum', EnumType::class);
+        Type::addType('customEnum', EnumType::class);
     }
 
     /**
@@ -486,7 +486,7 @@ class GenerateFactoryCommand extends Command
             return;
         }
 
-        $platform->registerDoctrineTypeMapping('enum', 'enum');
+        $platform->registerDoctrineTypeMapping('enum', 'customEnum');
         $platformName = $platform->getName();
         $customTypes = $this->laravel['config']->get("ide-helper.custom_db_types.{$platformName}", []);
 
